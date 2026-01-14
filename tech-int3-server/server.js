ed6be9a68d20a9c1912f7bf4c64fd53b59c44830
drainkid <elmirrzaevv@gmail.com>
2026-01-14 23:36:08 +1000

    change server routes

--- tech-int3-server/server.js
+++ tech-int3-server/server.js
@@ -6,7 +6,12 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(cors())
+const corsOptions = {
+  origin: process.env.CLIENT_URL || 'http://localhost:5173',
+  optionsSuccessStatus: 200 
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(express.static('public'));
