4b79f5441281b01fa7d37e61c59989dc39fae990
drainkid <elmirrzaevv@gmail.com>
2026-01-21 00:10:19 +1000

    bug fixes

--- src/api/api.ts
+++ src/api/api.ts
@@ -3,7 +3,7 @@ import type {GetAdvertsParams, StatsQueryParams} from "../types/advert.ts";
 
 
 const api = axios.create({
-    baseURL:  "http://localhost:3001/api/v1",
+    baseURL: import.meta.env.VITE_API_URL + 'api/v1',
 })
 
 
